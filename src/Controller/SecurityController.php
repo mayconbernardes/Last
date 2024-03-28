83cc24e2c961bf836ae61c8c28604fe16887dcff
mayconbernardes <mayconwin@hotmail.com>
2024-03-28 16:06:50 +0100

    modif

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -6,6 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
 
 class SecurityController extends AbstractController
 {
@@ -24,9 +26,12 @@ class SecurityController extends AbstractController
         return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
 
-    #[Route(path: '/logout', name: 'app_logout')]
-    public function logout(): void
-    {
-        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
-    }
+            #[Route(path: '/logout', name: 'app_logout')]
+        public function logout(): RedirectResponse
+        {
+            // No need to throw an exception or return anything else,
+            // Symfony will handle the redirection automatically.
+
+            return $this->redirectToRoute('app_index');
+        }
 }
